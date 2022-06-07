8248b0d9fbd5caf35c376579da0ac33d1c34ba26
mauro079476 <5090@holbertonschool.com>
2022-06-07 15:20:31 -0700

    task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0
+*/
+int main(void)
+{
+char i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	putchar(i);
+}
+return(0);
+}
+
