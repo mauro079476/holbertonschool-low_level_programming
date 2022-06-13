fc9350c957bf4ef5add27607cdffce96a87133b0
mauro079476 <5090@holbertonschool.com>
2022-06-12 19:22:44 -0700

    _putchar

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print from numbero 0 to 9
+ *
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		_putchar (i % 10 + '0');
+	}
+			_putchar ('\n');
+}
