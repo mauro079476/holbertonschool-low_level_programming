cb6d2e16f7063d459264b7300204482af3f897c7
mauro079476 <5090@holbertonschool.com>
2022-06-21 11:25:59 -0700

    task 1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -6,19 +6,9 @@
 */
 void _print_rev_recursion(char *s)
 {
-	int counter, i;
-
-	if (s[0] == '\0')
-		_putchar ('\n');
 	if (*s)
 	{
-		for (counter = 0; s[counter]; counter++)
-		{
-		}
-		i = (counter - 1);
-		for (; i >= 0; i--)
-		{
-			_putchar(s[i]);
-		}
-}
+		_print_rev_recursion(s + 1);
+			_putchar (*s);
+	}
 }
