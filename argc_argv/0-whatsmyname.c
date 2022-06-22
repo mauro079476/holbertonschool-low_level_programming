64d7160bc95ca5fcc25e358c236ee165c68e1b45
mauro079476 <5090@holbertonschool.com>
2022-06-21 18:52:51 -0700

    task 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/*
-* main - argv argc
+/**
+*main - argv argc
 *@argc: size
 *@argv: array
 *Return: 0
 */
-int main (__attribute__((unused))int argc, char *argv[])
+int main(__attribute__((unused))int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
