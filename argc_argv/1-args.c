1269d5a2484cd624463f62f6337cee6e784bd9dd
mauro079476 <5090@holbertonschool.com>
2022-06-21 19:04:56 -0700

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*main - argv argc
+*@argc: size
+*@argv: array
+*Return: 0
+*/
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
