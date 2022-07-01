20be256055938d9d077595b8a7f3a89004e19746
mauro079476 <5090@holbertonschool.com>
2022-07-01 13:33:11 -0700

    task

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+/**
+* print_numbers - print_numbers
+* @separator: separator
+* @n: int
+* Return: void
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list ptr;
+
+	if (n == 0)
+		return;
+	va_start(ptr, n);
+	if (separator == NULL)
+	{
+		for (; i < n; i++)
+		{
+			printf("%d", va_arg(ptr, int));
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (i = 0; i < n - 1; i++)
+		{
+			printf("%d%s", va_arg(ptr, int), separator);
+		}
+		printf("%d", va_arg(ptr, int));
+		printf("\n");
+	}
+}
