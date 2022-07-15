b562a1fe60a1c264cd475a23014136d8027892be
mauro079476 <5090@holbertonschool.com>
2022-07-15 16:38:26 -0700

    task 3

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -2,31 +2,29 @@
 /**
  * add_nodeint_end -  adds a new node at the end of a list
  * @n: int
+ * @head: first node
  * Return: ddress of the new element
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *newnode, *auxiliar = *head;
+	listint_t *newnode, *last = *head;
 
 	newnode = malloc(sizeof(listint_t));
-	if (auxiliar == NULL)
-		return (NULL);
 
-	auxiliar->n = n;
-
-	if (*head == NULL)
+	if (newnode)
 	{
-		*head = auxiliar;
-		auxiliar->next = NULL;
+		if (*head == NULL)
+			*head = newnode;
+		else
+		{
+				while (last->next != NULL)
+					last = last->next;
+			last->next = newnode;
+		}
+		newnode->n = n;
+		newnode->next = NULL;
 	}
-
-	newnode = *head;
-
-	while (newnode->next != NULL)
-	{
-		newnode = newnode->next;
-	}
-	newnode->next = auxiliar;
-	auxiliar->next = NULL;
-	return (auxiliar);
+	else
+		free(newnode);
+	return (newnode);
 }
