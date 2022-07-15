80d395a73311a491e40f25cdec471c6fdabe691a
mauro079476 <5090@holbertonschool.com>
2022-07-15 14:17:21 -0700

    task 0

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - print all the elements of a list
+ * @h: head
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int counter = 0;
+
+	while (h)
+	{
+		printf("[%d]\n", h->n);
+			counter++;
+			h = h->next;
+	}
+	return (counter);
+}
---
+++ more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+In this project im learning more deeply about linked lists and start to look for the right source of information without too much help
