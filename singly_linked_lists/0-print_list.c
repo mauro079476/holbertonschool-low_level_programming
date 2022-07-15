fce96b31d08221eca7d014c421a97397d6741cd7
mauro079476 <5090@holbertonschool.com>
2022-07-15 09:16:02 -0700

    task 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_list -  prints all the elements of a list_t
+ * @h: head
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int counter;
+
+	if (h == NULL)
+	return (0);
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+				counter++;
+				h = h->next;
+	}
+	return (counter);
+}
---
+++ singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+In this project im learning what are linked lists, how to use and build them.
