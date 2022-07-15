18a103335d4d246c40b95998673875011072be10
mauro079476 <5090@holbertonschool.com>
2022-07-15 16:58:05 -0700

    task 4

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - frees a list
+ * @head: head
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *freemem;
+
+	while (head)
+	{
+		freemem = head->next;
+		free(head);
+		head = freemem;
+	}
+}
