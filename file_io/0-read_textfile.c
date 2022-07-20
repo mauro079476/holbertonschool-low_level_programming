4697e0581a9380ccaae3a5c804b7b94d5d44c7e8
mauro079476 <5090@holbertonschool.com>
2022-07-20 15:23:27 -0700

    task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -20,7 +20,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	count = read(fd, buffer, letters);
-	write(1, buffer, count);
+	write(STDOUT_FILENO, buffer, count);
 
 	close(fd);
 	return (count);
