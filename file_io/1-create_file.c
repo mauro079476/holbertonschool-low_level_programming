1540f125be9de16b3daac2beaa9416f82c520b21
mauro079476 <5090@holbertonschool.com>
2022-07-20 17:04:36 -0700

    task 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* create_file - creates a file
+* @filename: name of file
+* @text_content: number of letters
+* Return: 1
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd, count = 0;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+
+	while (text_content && text_content[count])
+	{
+		count++;
+	}
+	count = write(fd, text_content, count);
+
+	close(fd);
+	return (1);
+}
+
