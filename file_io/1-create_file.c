aef36e8588453770ac1f5690c306d5d59a273b7f
mauro079476 <5090@holbertonschool.com>
2022-07-20 17:20:14 -0700

    task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,8 +14,8 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 
-	if (text_content == NULL)
-		return (-1);
+	if (text_content)
+
 	while (text_content && text_content[count])
 	{
 		count++;
