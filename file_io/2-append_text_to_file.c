db00c1bf1a93feb57e3b477c41f66aa350d068e7
mauro079476 <5090@holbertonschool.com>
2022-07-20 17:52:38 -0700

    task 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -9,15 +9,14 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, count = 0;
 
-	if (filename == NULL)
-		return (0);
+	if (!filename)
+		return (-1);
 
 	fd = open(filename, O_APPEND, O_WRONLY);
 	if (fd == -1)
 		return (-1);
-
 	while (text_content && text_content[count])
-			count++;
+		count++;
 
 	write(fd, text_content, count);
 	return (1);
