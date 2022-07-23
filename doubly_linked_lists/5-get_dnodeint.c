a7712d2a1cb06590ef25465ec4f7788071e2aebe
mauro079476 <5090@holbertonschool.com>
2022-07-22 18:14:57 -0700

    task 5

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -9,7 +9,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
-	for (i = 0; i < index && head; i++)
+	while (head)
 	{
 		if (i == index)
 			return (head);
