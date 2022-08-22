49a15ace5ef7bc0d105d961eebee42c03e4587ac
mauro079476 <5090@holbertonschool.com>
2022-08-21 18:11:34 -0700

    creating a hash table

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of table
+ * Return: Pointer to the new created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *htable = NULL;
+	unsigned long int i = 0;
+
+	if (size != 0)
+	{
+		htable = malloc(sizeof(hash_table_t));
+		if (!htable)
+		{
+			return (NULL);
+		}
+		htable->size = size;
+		htable->array = malloc(sizeof(hash_node_t *) * size);
+
+		if (!(htable->array))
+		{
+			free(htable);
+			return (NULL);
+		}
+		while (i < size)
+		{
+			htable->array[i] = NULL;
+			i++;
+		}
+		return (htable);
+	}
+	return (NULL);
+
+}
