7284df95d43f2b4a5409fb02846b30a10d1f020d
mauro079476 <5090@holbertonschool.com>
2022-08-21 19:59:47 -0700

    task 5

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,1 +1,32 @@
-aaaaaa
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that prints a hash table
+ * @ht: hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	char *print_comma = "";
+	hash_node_t *node = NULL;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+
+	printf("{");
+	while (i < ht->size)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			printf("%s'%s': '%s'", print_comma, node->key, node->value);
+			print_comma = ", ";
+			node = node->next;
+		}
+		i++;
+	}
+	printf("}\n");
+}
